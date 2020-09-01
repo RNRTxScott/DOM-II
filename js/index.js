bca41dc0a50b81871810a8d42d84fd14adaca6b7
Scott Vanderburg <s.vanderburg@realnerdsrt.com>
2020-09-01 15:29:32 -0600

    completed 3/10 events using just h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const mnFunHeader = document.querySelector('.logo-heading')
+
+mnFunHeader.addEventListener('mouseenter', () => {
+    mnFunHeader.style.transform = 'scale(1.5)';
+    mnFunHeader.style.transition = 'transform 0.4s'
+})
+mnFunHeader.addEventListener('mouseleave', () => {
+    mnFunHeader.style.transform = 'scale(1)'
+})

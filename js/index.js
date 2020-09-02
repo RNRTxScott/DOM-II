523601744b1b2d7968d528714162a17e81ac7175
Scott Vanderburg <s.vanderburg@realnerdsrt.com>
2020-09-01 21:35:03 -0600

    completed 10/10 event listeners of different types

--- js/index.js
+++ js/index.js
@@ -45,3 +45,26 @@ introNav.addEventListener('focusin',() => {
 introNav.addEventListener('focusout',() => {
     introNav.style.background = '';
 });
+
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
+// 9th
+window.addEventListener('resize', reportWindowSize);
+
+
+// 10th
+document.addEventListener('fullscreenchange', (event) => {
+    if (document.fullscreenElement) {
+      console.log(`Element: ${document.fullscreenElement.id} entered full-screen mode.`);
+    } else {
+      console.log('Leaving full-screen mode.');
+    }
+  });
+    
